6cb280253aa36d40cf13d390e202251f0be063f7
Patrick Poopathi <patrickgp1992@hotmail.com>
2022-09-04 09:13:54 -0400

    full screen testing

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -105,15 +105,37 @@ window.addEventListener("load", function () {
   }
 
   function toggleFullscreen() {
-    if (!document.webkitFullScreenElement) {
-      canvas.webkitRequestFullScreen().catch((err) => {
-        alert(`Error, can't enable full-screen mode: ${err.message}`);
-      });
-    } else if (document.exitFullscreen) {
-      document.exitFullscreen();
-    } else if (document.webkitExitFullScreen) {
-      document.webkitExitFullScreen();
+    var doc = window.document;
+    var docEl = doc.documentElement;
+
+    var requestFullScreen =
+      docEl.requestFullscreen ||
+      docEl.mozRequestFullScreen ||
+      docEl.webkitRequestFullScreen ||
+      docEl.msRequestFullscreen;
+    var cancelFullScreen =
+      doc.exitFullscreen ||
+      doc.mozCancelFullScreen ||
+      doc.webkitExitFullscreen ||
+      doc.msExitFullscreen;
+
+    if (
+      !doc.fullscreenElement &&
+      !doc.mozFullScreenElement &&
+      !doc.webkitFullscreenElement &&
+      !doc.msFullscreenElement
+    ) {
+      requestFullScreen.call(docEl);
+    } else {
+      cancelFullScreen.call(doc);
     }
+  }
+  if (!document.fullscreenElement) {
+    canvas.requestFullscreen().catch((err) => {
+      alert(`Error, can't enable full-screen mode: ${err.message}`);
+    });
+  } else if (document.exitFullscreen) {
+    document.exitFullscreen();
   }
 
   fullScreenButton.addEventListener("click", toggleFullscreen);
